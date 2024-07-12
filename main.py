fac276547a4c368a63e550fdbd0ac4c66cee270e
DalLidas <126471477+DalLidas@users.noreply.github.com>
2024-07-12 18:47:20 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import requests
+
+token = "6689b960e0e1b6689b960e0e1e"
+
+mainServer = "https://games.datsteam.dev"
+testServer = "https://games-test.datsteam.dev"
+
+turnTime = 1 # время одного хода
