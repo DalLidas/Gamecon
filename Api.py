7b7ade554413252fada7ca42bd35ab5ceca9b64a
DalLidas <126471477+DalLidas@users.noreply.github.com>
2024-07-12 20:18:23 +0300

    BaseApi

---
+++ Api.py
@@ -0,0 +1,147 @@
+import requests
+import json
+
+class Api:
+    def __init__(self, serverURL, token):
+        self.serverURL = serverURL
+        self.token = token
+
+        self.headers = {
+            "Authorization": f"Bearer {token}",
+            "Content-Type": "application/json"
+        }
+
+
+    def CommandAttack(self, attacked, build, moveBase):
+        data = json.JSONEncoder({"attack": attacked, "build": build, "moveBase": moveBase})
+        response = requests.post(self.serverURL+ "/lay/zombidef/command", headers=self.headers, json=data)
+        print(response)
+
+        # Content type: application/json
+        # {
+        #     "attack": [
+        #         {
+        #         "blockId": "f47ac10b-58cc-0372-8562-0e02b2c3d479",
+        #         "target": {
+        #             "x": 1,
+        #             "y": 1
+        #             }
+        #         }
+        #     ],
+        #     "build": [
+        #         {
+        #         "x": 1,
+        #         "y": 1
+        #         }
+        #     ],
+        #     "moveBase": {
+        #         "x": 1,
+        #         "y": 1
+        #     }
+        # }
+
+    def Participate(self):
+        response = requests.put(self.serverURL+ "/play/zombidef/participate", headers=self.headers)
+        print(response)
+
+        # Content type: application/json
+        # {
+        #     "startsInSec": 300
+        # }
+
+    def GetDynamicObjects(self):
+        response = requests.get(self.serverURL+ "/play/zombidef/units", headers=self.headers)
+        print(response)
+        return json.JSONDecoder(response)
+    
+        # {
+        #     "base": [
+        #         {
+        #         "attack": 10,
+        #         "health": 100,
+        #         "id": "f47ac10b-58cc-4372-a567-0e02b2c3d479",
+        #         "isHead": true,
+        #         "lastAttack": {
+        #             "x": 1,
+        #             "y": 1
+        #         },
+        #         "range": 5,
+        #         "x": 1,
+        #         "y": 1
+        #         }
+        #     ],
+        #     "enemyBlocks": [
+        #         {
+        #         "attack": 10,
+        #         "health": 100,
+        #         "isHead": true,
+        #         "lastAttack": {
+        #             "x": 1,
+        #             "y": 1
+        #         },
+        #         "name": "player-test",
+        #         "x": 1,
+        #         "y": 1
+        #         }
+        #     ],
+        #     "player": {
+        #         "enemyBlockKills": 100,
+        #         "gameEndedAt": "2021-10-10T10:00:00Z",
+        #         "gold": 100,
+        #         "name": "player-test",
+        #         "points": 100,
+        #         "zombieKills": 100
+        #     },
+        #     "realmName": "map1",
+        #     "turn": 1,
+        #     "turnEndsInMs": 1000,
+        #     "zombies": [
+        #         {
+        #         "attack": 10,
+        #         "direction": "up",
+        #         "health": 100,
+        #         "id": "f47ac10b-58cc-4372-a567-0e02b2c3d479",
+        #         "speed": 10,
+        #         "type": "normal",
+        #         "waitTurns": 1,
+        #         "x": 1,
+        #         "y": 1
+        #         }
+        #     ]
+        # }
+
+    def GetStaticObjects(self):
+        response = requests.get(self.serverURL+ "/play/zombidef/participate", headers=self.headers)
+        print(response)
+        return json.JSONDecoder(response)
+    
+        # {
+        #     "realmName": "map1",
+        #     "zpots": [
+        #         {
+        #         "x": 1,
+        #         "y": 1,
+        #         "type": "default"
+        #         }
+        #     ]
+        # }   
+
+    def GameRounds(self):
+        response = requests.get(self.serverURL+ "/rounds/zombidef", headers=self.headers)
+        print(response)
+
+        # Content type: application/json
+        # {
+        #     "gameName": "defense",
+        #     "now": "2021-01-01T00:00:00Z",
+        #     "rounds": [
+        #         {
+        #         "duration": 60,
+        #         "endAt": "2021-01-01T00:00:00Z",
+        #         "name": "Round 1",
+        #         "repeat": 1,
+        #         "startAt": "2021-01-01T00:00:00Z",
+        #         "status": "active"
+        #         }
+        #     ]
+        # }
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
 import json
 
+import Api
+
 token = "6689b960e0e1b6689b960e0e1e"
 
 mainServerURL = "https://games.datsteam.dev"
@@ -8,148 +10,10 @@ testServerURL = "https://games-test.datsteam.dev"
 
 turnTime = 1 # время одного хода
 
+def main():
+    api = Api(testServerURL, token)
 
-class Api:
-    def __init__(self, serverURL, token):
-        self.serverURL = serverURL
-        self.token = token
+    api.Participate()
 
-        self.headers = {
-            "Authorization": f"Bearer {token}",
-            "Content-Type": "application/json"
-        }
-
-
-    def CommandAttack(self, attacked, build, moveBase):
-        data = json.JSONEncoder({"attack": attacked, "build": build, "moveBase": moveBase})
-        response = requests.post(self.serverURL+ "/lay/zombidef/command", headers=self.headers, json=data)
-        print(response)
-
-        # Content type: application/json
-        # {
-        #     "attack": [
-        #         {
-        #         "blockId": "f47ac10b-58cc-0372-8562-0e02b2c3d479",
-        #         "target": {
-        #             "x": 1,
-        #             "y": 1
-        #             }
-        #         }
-        #     ],
-        #     "build": [
-        #         {
-        #         "x": 1,
-        #         "y": 1
-        #         }
-        #     ],
-        #     "moveBase": {
-        #         "x": 1,
-        #         "y": 1
-        #     }
-        # }
-
-    def Participate(self):
-        response = requests.put(self.serverURL+ "/play/zombidef/participate", headers=self.headers)
-        print(response)
-
-        # Content type: application/json
-        # {
-        #     "startsInSec": 300
-        # }
-
-    def GetDynamicObjects(self, ):
-        response = requests.get(self.serverURL+ "/play/zombidef/units", headers=self.headers)
-        print(response)
-        return json.JSONDecoder(response)
-    
-        # {
-        #     "base": [
-        #         {
-        #         "attack": 10,
-        #         "health": 100,
-        #         "id": "f47ac10b-58cc-4372-a567-0e02b2c3d479",
-        #         "isHead": true,
-        #         "lastAttack": {
-        #             "x": 1,
-        #             "y": 1
-        #         },
-        #         "range": 5,
-        #         "x": 1,
-        #         "y": 1
-        #         }
-        #     ],
-        #     "enemyBlocks": [
-        #         {
-        #         "attack": 10,
-        #         "health": 100,
-        #         "isHead": true,
-        #         "lastAttack": {
-        #             "x": 1,
-        #             "y": 1
-        #         },
-        #         "name": "player-test",
-        #         "x": 1,
-        #         "y": 1
-        #         }
-        #     ],
-        #     "player": {
-        #         "enemyBlockKills": 100,
-        #         "gameEndedAt": "2021-10-10T10:00:00Z",
-        #         "gold": 100,
-        #         "name": "player-test",
-        #         "points": 100,
-        #         "zombieKills": 100
-        #     },
-        #     "realmName": "map1",
-        #     "turn": 1,
-        #     "turnEndsInMs": 1000,
-        #     "zombies": [
-        #         {
-        #         "attack": 10,
-        #         "direction": "up",
-        #         "health": 100,
-        #         "id": "f47ac10b-58cc-4372-a567-0e02b2c3d479",
-        #         "speed": 10,
-        #         "type": "normal",
-        #         "waitTurns": 1,
-        #         "x": 1,
-        #         "y": 1
-        #         }
-        #     ]
-        # }
-
-    def GetStaticObjects(self, ):
-        response = requests.get(self.serverURL+ "/play/zombidef/participate", headers=self.headers)
-        print(response)
-        return json.JSONDecoder(response)
-    
-        # {
-        #     "realmName": "map1",
-        #     "zpots": [
-        #         {
-        #         "x": 1,
-        #         "y": 1,
-        #         "type": "default"
-        #         }
-        #     ]
-        # }   
-
-    def GameRounds(self):
-        response = requests.get(self.serverURL+ "/rounds/zombidef", headers=self.headers)
-        print(response)
-
-        # Content type: application/json
-        # {
-        #     "gameName": "defense",
-        #     "now": "2021-01-01T00:00:00Z",
-        #     "rounds": [
-        #         {
-        #         "duration": 60,
-        #         "endAt": "2021-01-01T00:00:00Z",
-        #         "name": "Round 1",
-        #         "repeat": 1,
-        #         "startAt": "2021-01-01T00:00:00Z",
-        #         "status": "active"
-        #         }
-        #     ]
-        # }
+if __name__ == "__main__":
+    main()
