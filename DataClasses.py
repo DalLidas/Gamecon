88e1031cb6da9ff7a763d27431800dfc7eed4128
alexexisted <a.assjones@list.ru>
2024-07-13 13:36:54 +0300

    UI v1

---
+++ DataClasses.py
@@ -0,0 +1,153 @@
+from dataclasses import dataclass
+from typing import Any
+from typing import List
+
+
+@dataclass
+class Zombie:
+    attack: int
+    direction: str
+    health: int
+    id: str
+    speed: int
+    type: str
+    waitTurns: int
+    x: int
+    y: int
+
+    @staticmethod
+    def from_dict(obj: Any) -> 'Zombie':
+        _attack = int(obj.get("attack"))
+        _direction = str(obj.get("direction"))
+        _health = int(obj.get("health"))
+        _id = str(obj.get("id"))
+        _speed = int(obj.get("speed"))
+        _type = str(obj.get("type"))
+        _waitTurns = int(obj.get("waitTurns"))
+        _x = int(obj.get("x"))
+        _y = int(obj.get("y"))
+        return Zombie(_attack, _direction, _health, _id, _speed, _type, _waitTurns, _x, _y)
+
+    def __iter__(self):
+        for value in self.__dict__.values():
+            yield value
+
+
+@dataclass
+class LastAttack:
+    x: int
+    y: int
+
+    @staticmethod
+    def from_dict(obj: Any) -> 'LastAttack':
+        _x = int(obj.get("x"))
+        _y = int(obj.get("y"))
+        return LastAttack(_x, _y)
+
+
+@dataclass
+class Base:
+    attack: int
+    health: int
+    id: str
+    isHead: bool
+    lastAttack: LastAttack
+    range: int
+    x: int
+    y: int
+
+    @staticmethod
+    def from_dict(obj: Any) -> 'Base':
+        _attack = int(obj.get("attack")) if obj.get("attack") is not None else 0
+        _health = int(obj.get("health"))
+        _id = str(obj.get("id"))
+        _isHead = bool(obj.get("isHead"))
+        _lastAttack = LastAttack.from_dict(obj.get("lastAttack"))
+        _range = int(obj.get("range"))
+        _x = int(obj.get("x"))
+        _y = int(obj.get("y"))
+        return Base(_attack, _health, _id, _isHead, _lastAttack, _range, _x, _y)
+
+    def __iter__(self):
+        for value in self.__dict__.values():
+            yield value
+
+
+@dataclass
+class EnemyBlock:
+    attack: int
+    health: int
+    isHead: bool
+    lastAttack: LastAttack
+    name: str
+    x: int
+    y: int
+
+    @staticmethod
+    def from_dict(obj: Any) -> 'EnemyBlock':
+        _attack = int(obj.get("attack"))
+        _health = int(obj.get("health"))
+        _isHead = bool(obj.get("isHead"))
+        _lastAttack = LastAttack.from_dict(obj.get("lastAttack"))
+        _name = str(obj.get("name"))
+        _x = int(obj.get("x"))
+        _y = int(obj.get("y"))
+        return EnemyBlock(_attack, _health, _isHead, _lastAttack, _name, _x, _y)
+
+    def __iter__(self):
+        for value in self.__dict__.values():
+            yield value
+
+
+@dataclass
+class Player:
+    enemyBlockKills: int
+    gameEndedAt: str
+    gold: int
+    name: str
+    points: int
+    zombieKills: int
+
+    @staticmethod
+    def from_dict(obj: Any) -> 'Player':
+        _enemyBlockKills = int(obj.get("enemyBlockKills"))
+        _gameEndedAt = str(obj.get("gameEndedAt"))
+        _gold = int(obj.get("gold"))
+        _name = str(obj.get("name"))
+        _points = int(obj.get("points"))
+        _zombieKills = int(obj.get("zombieKills"))
+        return Player(_enemyBlockKills, _gameEndedAt, _gold, _name, _points, _zombieKills)
+
+    def __iter__(self):
+        for value in self.__dict__.values():
+            yield value
+
+
+@dataclass
+class Units:
+    base: List[Base]
+    enemyBlocks: List[EnemyBlock]
+    player: Player
+    realmName: str
+    turn: int
+    turnEndsInMs: int
+    zombies: List[Zombie]
+
+    @staticmethod
+    def from_dict(obj: Any) -> 'Units':
+        _base = [Base.from_dict(y) for y in obj.get("base")]
+        _enemyBlocks = [EnemyBlock.from_dict(y) for y in obj.get("enemyBlocks")]
+        _player = Player.from_dict(obj.get("player"))
+        _realmName = str(obj.get("realmName"))
+        _turn = int(obj.get("turn"))
+        _turnEndsInMs = int(obj.get("turnEndsInMs"))
+        _zombies = [Zombie.from_dict(y) for y in obj.get("zombies")]
+        return Units(_base, _enemyBlocks, _player, _realmName, _turn, _turnEndsInMs, _zombies)
+
+    def __iter__(self):
+        for value in self.__dict__.values():
+            yield value
+
+# Example Usage
+# jsonstring = json.loads(myjsonstring)
+# root = Root.from_dict(jsonstring)
---
+++ UI.py
@@ -0,0 +1,59 @@
+import dearpygui.dearpygui as dpg
+
+import json
+from DataClasses import *
+
+dpg.create_context()
+dpg.create_viewport(title='Custom Title', width=1280, height=720)
+
+# Чтение данных из JSON файла
+with open('data.json') as f:
+    data = json.load(f)
+    UnitsData = Units.from_dict(data)
+    BaseData = [Base.from_dict(y) for y in data["base"]]
+    EnemyBlockdata = [EnemyBlock.from_dict(y) for y in data["enemyBlocks"]]
+    ZombieData = [Zombie.from_dict(y) for y in data["zombies"]]
+
+    print(data)
+
+    # Функция для отрисовки клетки
+    def draw_cell(x, y, color):
+        points = [[x, y], [x + 50, y], [x + 50, y + 50], [x, y + 50]]
+        dpg.draw_polygon(points, color=color, fill=color)
+
+# Создание окна
+with dpg.window(label="Main Window"):
+    # Отрисовка базы
+    for base_data in BaseData:
+        # head - yellow
+        draw_cell(base_data.x * 50, base_data.y * 50, [255, 255, 0])
+        # base - blue
+        draw_cell((base_data.x - base_data.range) * 50, (base_data.y - base_data.range) * 50,
+                  [16, 123, 185])
+
+        # Отрисовка зомби
+    for zombie in ZombieData:
+        if zombie.type == "bomber":
+            draw_cell(zombie.x * 50, zombie.y * 50, [255, 0, 0])
+        if zombie.type == "liner":
+            draw_cell(zombie.x * 50, zombie.y * 50, [255, 0, 213])
+        if zombie.type == "fast":
+            draw_cell(zombie.x * 50, zombie.y * 50, [94, 0, 255])
+        if zombie.type == "normal":
+            draw_cell(zombie.x * 50, zombie.y * 50, [159, 18, 18])
+        if zombie.type == "juggernaut":
+            draw_cell(zombie.x * 50, zombie.y * 50, [92, 85, 142])
+        if zombie.type == "chaos_knight":
+            draw_cell(zombie.x * 50, zombie.y * 50, [255, 84, 204])
+
+    # Отрисовка блоков врага
+    for enemy_block_data in EnemyBlockdata:
+        color = [0, 0, 0] if enemy_block_data.attack == 40 else [255, 255,
+                                                                    255]
+        draw_cell(enemy_block_data.x * 50, enemy_block_data.y * 50, color)
+
+# Запуск GUI
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.start_dearpygui()
+dpg.destroy_context()
